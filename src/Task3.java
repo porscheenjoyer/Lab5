bd5ef9c3da7826a12542ea3dfe5b6766b7c46320
Period 6 <braylen.diec@talawanda.org>
2024-02-15 11:40:37 -0500

    1140

--- src/Task3.java
+++ src/Task3.java
@@ -1,10 +1,51 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class Task3 {
     public static void main(String[] args) {
 
         //Variables
         Scanner in = new Scanner(System.in);
+        double userLength, userWidth, perimeter, area, diagonal, userDiagSquared;
+        String trash;
+
+        //collect dimensions
+        System.out.println("What is the length of your rectangle?");
+
+        if (in.hasNextDouble()) {
+
+            userLength = in.nextDouble();
+
+            System.out.println("What is the width of your rectangle?");
+
+            if (in.hasNextDouble()) {
+
+                userWidth = in.nextDouble();
+
+                perimeter = userLength * 2 + userWidth * 2;
+                area = userLength * userWidth;
+                userDiagSquared = (userWidth * userWidth) + (userLength * userLength);
+                diagonal = Math.sqrt(userDiagSquared);
+
+                System.out.println("The Perimeter: " + perimeter + "\nArea: " + area + "\nDiagonal: " + diagonal);
+
+            }
+
+            else {
+
+                trash = in.nextLine();
+                System.out.println("You entered an invalid value: " + trash);
+
+            }
+
+        }
+
+        else {
+
+            trash = in.nextLine();
+            System.out.println("You entered an invalid value: " + trash);
+
+        }
 
 
     }
